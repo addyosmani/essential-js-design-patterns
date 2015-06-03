ca31a50fcedd88228058bde069e80e8c7113f966
qcgm1978 <qcgm197874@gmail.com>
2015-06-03 18:39:17 +0800

    revert

--- test/jsTestDriver/book-code.js
+++ test/jsTestDriver/book-code.js
@@ -1,28 +1,22 @@
 var myModule = {
-
     myProperty: "someValue",
-
     // object literals can contain properties and methods.
     // e.g we can define a further object for module configuration:
     myConfig: {
         useCaching: true,
         language: "en"
     },
-
     // a very basic method
     saySomething: function () {
         return ( "Where in the world is Paul Irish today?" );
     },
-
     // output a value based on the current configuration
     reportMyConfig: function () {
         return ( "Caching is: " + ( this.myConfig.useCaching ? "enabled" : "disabled") );
     },
-
     // override the current configuration
-    updateMyConfig: function( newConfig ) {
-
-        if ( typeof newConfig === "object" ) {
+    updateMyConfig: function (newConfig) {
+        if (typeof newConfig === "object") {
             this.myConfig = newConfig;
             return ( this.myConfig.language );
         }
@@ -31,4 +25,25 @@ var myModule = {
         return this
     }
 };
+//module pattern
+var modulePattern = (function () {
+
+    var counter = 0;
+
+    return {
+
+        incrementCounter: function () {
+            return ++counter;
+        },
+
+        resetCounter: function () {
+            counter = 0;
+            return ( "counter value prior to reset: " + counter );
+        },
+        getCounter: function () {
+            return counter;
+        }
+    };
+
+})();
 
--- test/jsTestDriver/test-case.js
+++ test/jsTestDriver/test-case.js
@@ -1,15 +1,26 @@
-ObjectModule = TestCase("GreeterTest");
+ObjectModule = TestCase("ObjectTest");
 ObjectModule.prototype.testGreet = function () {
     var say1 = myModule.saySomething();
-    var say2=myModule.reportMyConfig();
-    var say3=myModule.updateMyConfig({
+    var say2 = myModule.reportMyConfig();
+    var say3 = myModule.updateMyConfig({
         language: "fr",
         useCaching: false
     });
-    var say4=myModule.reportMyConfig();
+    var say4 = myModule.reportMyConfig();
     assertEquals('Where in the world is Paul Irish today?', say1);
-    assertEquals('Caching is: enabled',say2)
-    assertEquals('fr',say3)
-    assertEquals('Caching is: disabled',say4)
-    assertEquals(myModule,myModule.retThis())
+    assertEquals('Caching is: enabled', say2)
+    assertEquals('fr', say3)
+    assertEquals('Caching is: disabled', say4)
+    assertEquals(myModule, myModule.retThis())
+};
+ObjectModule = TestCase("GreeterTest");
+ObjectModule.prototype.testGreet = function () {
+    var newCounter = modulePattern.incrementCounter();
+    var newCounter1 = modulePattern.resetCounter();
+    assertEquals(1, newCounter);
+    assertEquals('counter value prior to reset: 0', newCounter1)
+    var counter = Infinity
+    assertEquals(Infinity, counter)
+    //variable in object accessed firstly
+    assertNotEquals(Infinity,modulePattern.getCounter())
 };
