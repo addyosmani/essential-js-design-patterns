4087236d3d0acd92c01479db344ea16dd2192b2d
Dmitrii Pashutskii <guar47@ya.ru>
2018-02-20 12:32:16 +0300

    add 26 chapter

---
+++ book/snippets/04-Design-Patterns-In-jQuery/26-the-iterator-pattern.es5.js
@@ -0,0 +1,55 @@
+//********************** Snippet 1 **********************//
+$.each( ["john","dave","rick","julian"], function( index, value ) {
+  console.log( index + ": " + value);
+});
+ 
+$( "li" ).each( function ( index ) {
+  console.log( index + ": " + $( this ).text());
+});
+
+//********************** Snippet 2 **********************//
+// Execute a callback for every element in the matched set.
+each: function( callback, args ) {
+  return jQuery.each( this, callback, args );
+}
+
+//********************** Snippet 3 **********************//
+each: function( object, callback, args ) {
+  var name, i = 0,
+    length = object.length,
+    isObj = length === undefined || jQuery.isFunction( object );
+ 
+  if ( args ) {
+    if ( isObj ) {
+      for ( name in object ) {
+        if ( callback.apply( object[ name ], args ) === false ) {
+          break;
+        }
+      }
+    } else {
+      for ( ; i < length; ) {
+        if ( callback.apply( object[ i++ ], args ) === false ) {
+          break;
+        }
+      }
+    }
+ 
+  // A special, fast, case for the most common use of each
+  } else {
+    if ( isObj ) {
+      for ( name in object ) {
+        if ( callback.call( object[ name ], name, object[ name ] ) === false ) {
+          break;
+        }
+      }
+    } else {
+      for ( ; i < length; ) {
+        if ( callback.call( object[ i ], i, object[ i++ ] ) === false ) {
+          break;
+        }
+      }
+    }
+  }
+ 
+  return object;
+};
