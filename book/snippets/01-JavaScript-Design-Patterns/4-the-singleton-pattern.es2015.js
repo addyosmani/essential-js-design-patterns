ad1d4e661a4d3b25bc612daa057bfec124dc2426
Dmitrii Pashutskii <guar47@ya.ru>
2018-03-04 12:15:33 +0300

    fix singleton

--- book/snippets/01-JavaScript-Design-Patterns/4-the-singleton-pattern.es2015.js
+++ book/snippets/01-JavaScript-Design-Patterns/4-the-singleton-pattern.es2015.js
@@ -15,34 +15,34 @@ let instance;
 const privateMethod = () => {
     console.log('I am private');
   };
-  const privateVariable = 'Im also private';
+const privateVariable = 'Im also private';
+const randomNumber = Math.random();
   
-  // Singleton
-  class MySingleton {
-    // Get the Singleton instance if one exists
-    // or create one if it doesn't
-    constructor() {
-      if (!instance) {
-        // Public property
-        this.randomNumber = Math.random();
-        this.publicProperty = 'I am also public';
-        instance = this;
-      }
-  
-      return instance;
+// Singleton
+class MySingleton {
+  // Get the Singleton instance if one exists
+  // or create one if it doesn't
+  constructor() {
+    if (!instance) {
+      // Public property
+      this.publicProperty = 'I am also public';
+      instance = this;
     }
-  
-    // Public methods
-    publicMethod() {
-      console.log('The public can see me!');
-    }
-  
-    getRandomNumber() {
-      return this.randomNumber;
-    }
+
+    return instance;
   }
-  // [ES2015+] Default export module, without name
-  export default MySingleton;
+
+  // Public methods
+  publicMethod() {
+    console.log('The public can see me!');
+  }
+
+  getRandomNumber() {
+    return randomNumber;
+  }
+}
+// [ES2015+] Default export module, without name
+export default MySingleton;
   
 
 // Instance stores a reference to the Singleton
